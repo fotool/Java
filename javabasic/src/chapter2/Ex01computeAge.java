49e29a92192838672ebe1f95960117fd9ae5516a
fotool <50321812+fotool@users.noreply.github.com>
2019-07-11 17:23:45 +0900

    Java basic

--- javabasic/src/chapter2/Ex01computeAge.java
+++ javabasic/src/chapter2/Ex01computeAge.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Ex01computeAge {
 
 	public static void main(String[] args) {
+		// 필요한 변수 선언
 		Scanner sc = new Scanner(System.in);
-		String name = "이름을 입력해 주세요 :";
-		System.out.println(name);
-		// System.out.println("이름을 입력해 주세요 : ");
-		name = sc.next();
-		System.out.println(name);
-		System.out.println("태어난 연도를 입력해 주세요 : ");
-	    double age = sc.nextDouble();
-	    double age = 2019 - birthday;
-	    System.out.println((int)age);
+		String name ;
+		int age, birthYear;
 		
+		//키보드를 통한 입력
+		System.out.println("이름을 입력해 주세요 : ");
+		name = sc.next();
+		System.out.println("태어난 연도를 입력해 주세요 : ");
+		birthYear = sc.nextInt();
+		
+		// 나이 계산
+	    age = 2019 - birthYear;
+		
+	    // 화면상에 출력
+	    System.out.println(name + "님의 나이는" + age + "세 입니다.");
 	}
 
 }
