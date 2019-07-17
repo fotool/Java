341524d334dcd3ae1d4eb8c382bfd872fce3d482
GitHubEunJi <52555334+GitHubEunJi@users.noreply.github.com>
2019-07-17 16:18:36 +0900

    while

--- javabasic/src/chapter4/A05_WhileStmt.java
+++ javabasic/src/chapter4/A05_WhileStmt.java
@@ -28,11 +28,29 @@ public class A05_WhileStmt {
 		 * System.out.println(num); num++;
 		 */
 		// 위 프로그램을 반복문을 사용하여 작성하면....
-		int num = 1;
-		while(num<=10000) {
-			System.out.println(num);
-			num++;
-		}
+		/*
+		 * int num = 1; while(num<=10000) { System.out.println(num); num++; }
+		 */
+		
+		// 1-100까지의 수에서 홀수, 짝수의 합을 구하시오.
+		//Pseudo code
+		// 1. 필요한 변수 선언
+		// 2. 숫자가 100보다 작은지 확인
+		//   2.1 숫자가 홀수이면 홀수의 합
+		//   2.2 짝수이면 짝수의 합이
+		//   2.3 숫자를 증가시킨다.
+		// 3. 홀수의 합계 출력
+		// 4. 짝수의 합계 출력
+		
+		// 변수 선언
+		/*
+		 * int num =1 , oddsum = 0, evensum = 0; while(num<=100) { if((num % 2) == 0)
+		 * evensum += num; else oddsum += num; num ++; }// end of while
+		 * 
+		 * System.out.println("oddsum :" + oddsum); System.out.println("evensum :" +
+		 * evensum);
+		 */
+		
 	}
 
 }
