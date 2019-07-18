f27e0da0b68ebbc568d128fd3ffd4879a3c24dfb
fotool <50321812+fotool@users.noreply.github.com>
2019-07-18 22:04:59 +0900

    y

--- javabasic/src/practice/practice2.java
+++ javabasic/src/practice/practice2.java
@@ -3,8 +3,13 @@ package practice;
 public class practice2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "Hello World";
+		String anotherString = "hello world";
+		Object objStr = str;
+		
+		System.out.println( str.compareTo(anotherString));
+		System.out.println( str.compareToIgnoreCase(anotherString));
+		System.out.println( str.compareTo(objStr.toString()));
 	}
 
 }
